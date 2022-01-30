0cc07fe8b5685ac138b3deb4f851b643e51f58d7
CHANEL <71224645+ChanelBerma108@users.noreply.github.com>
2022-01-30 16:38:54 +0200

    Vid 57 Done

--- src/questions/pages/Questions.js
+++ src/questions/pages/Questions.js
@@ -3,7 +3,13 @@ import QuestionsList from "../components/QuestionsList";
 const Questions = () => 
 {
     const QUESTIONS = [{id : 'q1', q: 'What is your favourite color ? ' , image: 'https://media.istockphoto.com/photos/green-number-1-picture-id173943775'  }, 
-     {id : 'q2', q: 'What kind of pets do u have ?' , image: 'https://media.istockphoto.com/photos/fun-number-two-picture-id883061952', places: 7  }] ; 
+     {id : 'q2', q: 'What kind of pets do u have ? ' , image: 'https://media.istockphoto.com/photos/fun-number-two-picture-id883061952'  },
+     {id : 'q3', q: 'Are you male or female ? ' , image: ''  },
+     {id : 'q4', q: 'What is your age ? ' , image: ''  },
+     {id : 'q4', q: 'Select the foods that you like from the list? ' , image: ''  }
+
+     
+    ] ; 
     return <QuestionsList items = {QUESTIONS} />; 
 }; 
 export default Questions ; 
